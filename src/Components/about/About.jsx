909fd777b5a739228e335b013d536feea0b11ed6
Parva Shah <parvashah310501@gmail.com>
2023-02-13 13:26:54 -0500

    About

--- src/Components/about/About.jsx
+++ src/Components/about/About.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './about.css'
+import ME from '../../Assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { VscFolderLibrary } from 'react-icons/vsc'
 
 const About = () => {
   return (
@@ -9,10 +12,39 @@ const About = () => {
 
       <div className="container about__container">
         <div className="about__me">
-
+          <div className="about__me-image">
+            <img src={ME} alt="My Image" />
+          </div>
         </div>
         
         <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon"/>
+              <h5>Experience</h5>
+              <small>2+Years Working</small>
+            </article>
+
+            {/* <article className="about__card">
+              <FaAward className="about__icon"/>
+              <h5></h5>
+              <small></small>
+            </article> */}
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon"/>
+              <h5>Projects</h5>
+              <small>20+ Projects</small>
+            </article>
+          </div>
+
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing 
+              elit. Quo ullam, ad, tempora magni amet in pariatur praesentium illum fugiat atque est molestiae laboriosam dolore autem, provident non
+              officia neque iusto.
+            </p>
+
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
 
         </div>
       </div>
